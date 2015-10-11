463d3a1a38cc51bf666200bb6e25b2adc5ff2224
Drew J. Sonne <drew.sonne@nordcloud.com>
2015-10-11 20:02:59 +0200

    Added parser classes.

--- pyvagrantconfig/__init__.py
+++ pyvagrantconfig/__init__.py
@@ -1,3 +1,23 @@
 __author__ = 'drews'
 
-class Vagrantfile(object): pass
+class Vagrantfile(object):
+
+    def __init__(self):
+        self.struct = None
+
+    def __getattr__(self, name):
+        return getattr(self.struct, name)
+
+
+class VagrantfileVm(object): pass
+
+class VagrantfileProviderVb(object): pass
+
+class VagrantfileNetworkForwardedPort(object):
+    def __init__(self, guest, host):
+        self.guest = int(guest)
+        self.host = int(host)
+
+class VagrantfileNetworkPrivateNetwork(object):
+    def __init__(self, ip=None):
+        self.ip = ip
