801e73a7281f62297ee166dbf211fbbdb8ccf6ee
Drew J. Sonne <drew.sonne@nordcloud.com>
2015-10-14 08:19:57 +0100

    Filled out setup.py metadata

--- setup.py
+++ setup.py
@@ -10,14 +10,34 @@ requirements = [ ]
 if sys.version_info[:2] == (2, 6):
     requirements.append('ordereddict')
 
-
 setup(
     name='pyvagrantconfig',
-    version='0.1',
     packages=find_packages(exclude=['tests*']),
+    version='0.5',
+    description='Parser to extract data from a Vagrantfile into a data struct readable by python.',
+    author='Drew J. Sonne',
+    author_email='drew.sonne@gmail.com',
+    url='https://github.com/drewsonne/pyvagrantconfig',
     include_package_data=True,
     install_requires=requirements,
-    author_email='drew.sonne@gmail.com',
-    author='Drew J. Sonne',
-    url='https://github.com/drewsonne/pyvagrant-config'
+    keywords=['vagrant','parser','ruby'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development',
+        'Topic :: System :: Installation/Setup'
+    ]
 )
