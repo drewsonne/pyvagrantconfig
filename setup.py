539ccc8824914a872d3eb80ffc6981ca38380630
Drew J. Sonne <drew.sonne@nordcloud.com>
2015-10-18 19:58:49 +0200

    Added python2 classifier.

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Natural Language :: English',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
